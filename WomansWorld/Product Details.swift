0c20a171907e139cf923a2d0b599d6b2e425e625
sejalpatil30 <spatil3@babson.edu>
2022-06-15 15:32:42 -0400

    Search Results Product Details

---
+++ WomansWorld/Product Details.swift
@@ -0,0 +1,29 @@
+//
+//  Product Details.swift
+//  WomansWorld
+//
+//  Created by Sejal Shahaji Patil on 15/06/22.
+//
+
+import UIKit
+
+class Product_Details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
